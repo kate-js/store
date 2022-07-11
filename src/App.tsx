9848cd0c481f9559dffa4f9b0bafb3f4cb1f5397
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-11 14:31:41 +0200

    feat: add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-       
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
