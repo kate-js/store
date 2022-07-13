3de0a0e6303e4c406f948a0710860c4d76bb3f8c
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-13 10:57:17 +0200

    refactor: change CardItems

--- src/components/main/cardsList/card/CardItems.tsx
+++ src/components/main/cardsList/card/CardItems.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Card } from '../../../../types';
+import MyButton from '../../../UI/button/MyButton';
 import './CardItems.css';
 
 const CardItems = (props: Card) => {
   return (
     <div className='card'>
-      <h3>{props.card.title}</h3>
-      <span>{props.card.category}</span>
-      <span>{props.card.description}</span>
-      <span>{props.card.title}</span>
-      <img src={`${props.card.image}`} />
-      <span>{props.card.price}</span>
-      <span>{props.card.rating.count}</span>
-      <span>{props.card.rating.rate}</span>
+      <div className='card__content'>
+        <h3>{props.card.title}</h3>
+        <p>{props.card.category}</p>
+        <p>{props.card.description}</p>
+        <div className='card__image'>
+          <img src={`${props.card.image}`} alt='basket' />
+        </div>
+        <p>Цена: {props.card.price}</p>
+        <p>Рейтинг: {props.card.rating.count}</p>
+        <p>Количество отзывов: {props.card.rating.rate}</p>
+      </div>
+      <MyButton>В корзину</MyButton>
     </div>
   );
 };
