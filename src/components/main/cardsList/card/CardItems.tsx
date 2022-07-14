c04132ef7bcfbafd2dce027c84d303009c55a753
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-14 16:58:41 +0200

    refactor: add manufacturer, swap value

--- src/components/main/cardsList/card/CardItems.tsx
+++ src/components/main/cardsList/card/CardItems.tsx
@@ -8,14 +8,12 @@ const CardItems = (props: Card) => {
     <div className="card">
       <div className="card__content">
         <h3>{props.card.name}</h3>
-        <p>Количество: {props.card.count}</p>
-        <p>Год выпуска: {props.card.year}</p>
-        {/* <div className='card__image'>
-          <img src={`${props.card.image}`} alt='basket' />
-        </div> */}
         <p>Цвет: {props.card.color}</p>
         <p>Размер: {props.card.size}</p>
         <p>Форма: {props.card.shape}</p>
+        <p>Количество: {props.card.count}</p>
+        <p>Год выпуска: {props.card.year}</p>
+        <p>Производитель: {props.card.manufacturer}</p>
       </div>
       <MyButton>В корзину</MyButton>
     </div>
