3d14db65d563c88359341b35f16ed78abe8dd7a4
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-16 13:32:24 +0200

    feat: add info to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,14 @@
 import './footer.css';
-
+import LogoSchool from '../../assets/rss_logo.svg';
 const Footer = () => {
   return (
-    <footer className='footer'>
+    <footer className="footer">
+      <a href="https://github.com/kate-js" className="footer__github">
+        <span>GitHub: kate-js</span>
+      </a>
       <span>2022</span>
-      <a href='https://rs.school/js/' className='footer__logo'>
-        <img src='../../../assets/rss_logo.svg' />
+      <a href="https://rs.school/js/" className="footer__logo">
+        <img src={LogoSchool} alt="logo RSSchool" />
       </a>
     </footer>
   );
