7a921779f6e71f6659c61db1c2c3200c783e3a83
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-16 13:19:23 +0200

    refactor: changed selector style

--- src/components/UI/MySelect.tsx
+++ src/components/UI/MySelect.tsx
@@ -1,9 +1,10 @@
 import { IMySelect, IOption } from '../../types';
+import './MySelect.css';
 
 const MySelect = ({ options, defaultValue, value, onChange }: IMySelect) => {
   return (
-    <select value={value} onChange={(event) => onChange(event.target.value)}>
-      <option value='' disabled>
+    <select value={value} onChange={(event) => onChange(event.target.value)} className="main__select">
+      <option value="" disabled>
         {defaultValue}
       </option>
       {options.map((option: IOption) => (
